e77a67258c99a7f78d1364787b5eeb118759a28c
NeedSomeKnowlege <evgeniy.ushakov@gmail.com>
2018-11-20 12:46:37 +0200

    Done with chrome_login.rb

--- chrome_login.rb
+++ chrome_login.rb
@@ -7,20 +7,29 @@ browser = Selenium::WebDriver.for(:chrome)
 browser.navigate.to("https://www.google.com")
 puts "Google main page is opened."
 
-sleep 1 #wait 1 second
+sleep 3 #wait 3 sec.
 
 browser.find_element(:id,"gb_70").click
 puts "Login button is clicked."
+sleep 1 #wait 1 sec.
 
-browser.find_element(:id, "identifierId").send_keys("evgenii.ushakov@Fuseuniversal.com")
-puts "Email was written"
+browser.find_element(:id, "identifierId").send_keys("evgenii.ushakov")
+puts "Email was written."
+sleep 1 #wait 1 sec.
 
-browser.find_element(:class = RveJvd snByac, "Далее").click
-puts "Login button is clicked."
+browser.find_element(:id, "identifierNext").click
+puts"Next button is clicked."
 
-browser.find_element(:name, "password")).send_keys("15963")
+sleep 2 #wait 2 sec.
+
+browser.find_element(:css,"input[name=\"password\"]").send_keys("159632")
 puts "Password was written."
+sleep 2 #wait 2 sec.
 
-browser.find_element(:xpath,//*[@id="passwordNext"]/content/span).click
+browser.find_element(:css, 'span.RveJvd.snByac').click
 puts "Button NEXT was clicked."
-#expect(driver.page_source).to include("Invalid email or password")
+
+sleep 2 #wait 2 sec.
+
+browser.find_element(:css,"#password > div.LXRPh > div.dEOOab.RxsGPe")
+puts "Not valid login was used."
