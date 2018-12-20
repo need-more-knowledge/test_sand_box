dd5539211cd2d69f500c4790a4db2107e26eb4fe
NeedSomeKnowlege <evgeniy.ushakov@gmail.com>
2018-12-21 00:11:51 +0200

    fire-fox FB login

--- fire_fox.rb
+++ fire_fox.rb
@@ -5,5 +5,16 @@ Selenium::WebDriver.logger.level = :debug
 driver = Selenium::WebDriver.for :firefox
 puts "FF browser was opened."
 
+
 driver.navigate.to "https://www.facebook.com"
 puts "Facebook loading done on FF"
+
+#browser.find_element(css: "input[name ='email']").send_keys("Chack@norris.com")
+driver.find_element(:css, 'input.inputtext').send_keys("Chack@norris.com")
+puts "Email was written"
+
+driver.find_element(:xpath,'//*[@id="pass"]').send_keys(5262659)
+puts "Pssword was written"
+
+
+driver.quit
