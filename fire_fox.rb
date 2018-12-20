f6e558f9293b9d0b4f16ef126d7ceeb1a5458e66
NeedSomeKnowlege <evgeniy.ushakov@gmail.com>
2018-12-21 01:47:50 +0200

    not valid login FB

--- fire_fox.rb
+++ fire_fox.rb
@@ -13,8 +13,17 @@ puts "Facebook loading done on FF"
 driver.find_element(:css, 'input.inputtext').send_keys("Chack@norris.com")
 puts "Email was written"
 
-driver.find_element(:xpath,'//*[@id="pass"]').send_keys(5262659)
-puts "Pssword was written"
+driver.find_element(:xpath,'//*[@id="pass"]').send_keys(15963547)
+puts "Password was written"
+sleep 5
 
+driver.find_element(:id, "loginbutton").click
+puts "Login btn was clicked"
+sleep 5
+
+driver.find_element(:link, "Sign up for an account.")
+puts "Pop-up for unvalid login was shown"
+
+sleep 5
 
 driver.quit
