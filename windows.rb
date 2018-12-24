2ce6ec7db323677c0185a5aab44516596f8450f0
NeedSomeKnowlege <evgeniy.ushakov@gmail.com>
2018-12-24 17:03:48 +0200

    Tabs handling on FF

---
+++ windows.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'selenium-webdriver'
+Selenium::WebDriver.logger.level = :debug
+@driver = Selenium::WebDriver.for :firefox
+puts "FF browser was opened."
+
+@driver.get('http://the-internet.herokuapp.com/windows') #driver.navigate().back();driver.navigate().forward();driver.navigate().refresh()
+puts "site for testing was opened"
+sleep (4)
+
+main_window = @driver.window_handle
+@driver.find_element(link: 'Click Here').click
+puts "Link for new window opening was clicked."
+
+windows = @driver.window_handles
+  windows.each do |window|
+    if main_window != window
+      @new_window = window
+    end
+  end
+
+@driver.find_element(:class,"example")
+puts "Element on a new window was found"
+
+
+sleep (6)
+
+@driver.switch_to.window(main_window)
+puts "Switched to the main window"
+sleep (5)
+
+@driver.find_element(:link,"Elemental Selenium")
+puts"Link was found on a main page"
+sleep (4)
+
+@driver.quit()
