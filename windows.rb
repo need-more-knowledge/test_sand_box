d57d484df34d73b08320e63a70e5f6f871171155
NeedSomeKnowlege <evgeniy.ushakov@gmail.com>
2019-01-11 17:25:23 +0200

    adding a pause for new window

--- windows.rb
+++ windows.rb
@@ -6,12 +6,31 @@ puts "FF browser was opened."
 
 @driver.get('http://the-internet.herokuapp.com/windows') #driver.navigate().back();driver.navigate().forward();driver.navigate().refresh()
 puts "site for testing was opened"
-sleep (4)
+
 
 main_window = @driver.window_handle
 
 @driver.find_element(:link, 'Click Here').click
 puts "Link for new window opening was clicked."
+sleep 5 #wait for new window will open
+=begin
+def wait_until_true(timeout = 2)
+   start = Time.now
+   max = Time.now + timeout
+   loop do
+      res = yield
+      break if res || Time.current > max
+      sleep 0.02
+   end
+   raise "Timeout waiting for block to return true" unless res
+   res
+end
+(edited)
+
+добавляешь такой хелпер метод
+и используешь его так
+wait_until_true(5) { @driver.window_handles.size > 1  }
+=end
 
 windows = @driver.window_handles
   windows.each do |window|
@@ -19,23 +38,22 @@ windows = @driver.window_handles
       @new_window = window
     end
   end
-
+puts @new_window.class
 @driver.switch_to.window(@new_window)
 
 puts @driver.find_element(:class,"example").text
-sleep 5
+
 puts "Element on a new window was found"
 
-sleep (3)
 @driver.close()
-sleep (3)
+
 
 @driver.switch_to.window(main_window)
 puts "Switched to the main window"
-sleep (5)
+
 
 @driver.find_element(:link,"Elemental Selenium")
 puts"Link was found on a main page"
-sleep (4)
+
 
 @driver.quit()
