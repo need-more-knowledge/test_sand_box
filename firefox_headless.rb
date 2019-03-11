f1a391d246ab89c4b2dc883469e4b6cebdb25162
NeedSomeKnowlege <evgeniy.ushakov@gmail.com>
2019-03-11 15:12:14 +0200

    add notification to Telegram bot

--- firefox_headless.rb
+++ firefox_headless.rb
@@ -8,4 +8,9 @@ driver.get('http://stackoverflow.com/')
 
 puts driver.title
 
+if driver.title == 'Stack Overflow - Where Developers Learn, Share, & Build Careers' #send message to your bot
+  driver.get('https://api.telegram.org/botBOT_TOKEN/sendMessage?chat_id=CHAT_ID&text=Hello+World')
+else
+  puts 'Non-corresponding page title is shown'
+end
 driver.quit
