7a04c1529c17640e99e6b7695f6ec3f438c9be25
NeedSomeKnowlege <evgeniy.ushakov@gmail.com>
2019-06-20 13:41:37 +0300

    no more pauses

--- popup_hoover_ch.rb
+++ popup_hoover_ch.rb
@@ -3,19 +3,19 @@ require 'selenium-webdriver'
 Selenium::WebDriver.logger.level = :debug
 $driver = Selenium::WebDriver.for :chrome
 puts "Chrome browser was opened."
-sleep 5
+
 
 $driver.get'https://www.facebook.com/'
 puts "Facebook was opened"
 
 $driver.action.move_to($driver.find_element(:id,"birthday-help")).perform
 puts "Popup was shown by hoover over the element"
-sleep 6
+
 
 $driver.find_element(:id,"u_0_6")
 
 puts "ELEMENT ON POPUP WAS FOUND"
-sleep 6
+
 
 
 $driver.quit()
